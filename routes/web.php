2399765d41a90eb874b52f6fceaccd9a594b9c05
radhiah <sitiradhiahmegat@gmail.com>
2025-04-04 21:11:51 +0800

    first migrate api product and review

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResource('products', ProductController::class);
+
